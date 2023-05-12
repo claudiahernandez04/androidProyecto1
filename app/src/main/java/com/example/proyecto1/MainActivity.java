51b6241ef8b045beb482e8318206891d333f0107
Masterchief <luis.marquez3@utp.ac.pa>
2023-05-12 00:44:04 -0500

    Cambios completos

--- app/src/main/java/com/example/proyecto1/MainActivity.java
+++ app/src/main/java/com/example/proyecto1/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.proyecto1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_Ingresar = findViewById(R.id.btnIngresar);
+        btn_Ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Home.class);
+                startActivity(intent);
+            }
+        });
     }
 }
